d72e3f00b283d998ab5ea4f81870f3f5b7ea43fa
bogieganilaga <bogieganilaga.id@gmail.com>
2020-05-18 07:57:01 +0800

    Memperbarui publisher dan subscriber

--- mqtt/publisher.py
+++ mqtt/publisher.py
@@ -3,39 +3,49 @@
 
 import paho.mqtt.client as mqttClient
 import time
- 
+
+
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print('Connected to broker')
-        global Connected                #Use global variable
-        Connected = True                #Signal connection 
+        global Connected  # Use global variable
+        Connected = True  # Signal connection
     else:
         print('Connection failed')
- 
-Connected = False   #global variable for the state of the connection
 
-# After you successfully the publisher and subscribe code, 
+
+Connected = False  # global variable for the state of the connection
+
+# After you successfully the publisher and subscribe code,
 # try to create your own MQTT broker by create an account at cloudmqtt.com
-broker_address= 'your cloud broker url'
-port = 'your cloud broker port' # do not type string, but change to integer
-username = 'your username'
-password = 'your password'
- 
-client = mqttClient.Client('Python')               #create new instance
-client.username_pw_set(user, password=password)    #set username and password
-client.on_connect= on_connect                      #attach function to callback
-client.connect(broker_address, port=port)          #connect to broker
- 
-client.loop_start()        #start the loop
- 
-while Connected != True:    #Wait for connection
-    time.sleep(0.1)
- 
+broker_address = 'mqtt.eclipse.org'
+port = 1883  # do not type string, but change to integer
+# username = 'your username'
+# password = 'your password'
+
+client = mqttClient.Client()  # create new instance
+# client.username_pw_set(user, password=password)  # set username and password
+client.on_connect = on_connect  # attach function to callback
+client.connect(broker_address, port=port)  # connect to broker
+
+client.loop_start()  # start the loop
+
+while Connected != True:  # Wait for connection
+    time.sleep(2)
+
 try:
-    while True: 
-        value = input('Enter the message:')
-        client.publish('python/', value)
- 
+    while True:
+        # value = input('Enter the message:')
+        client.publish('python/CA171/mqtt', 'Bogie 170010020')
+        client.publish('python/CA171/socket', 'Bogie 170010020')
+        client.publish('python/CA171/email', 'Bogie 170010020')
+        client.publish('python/AA171/mqtt', 'Bogie 170010020')
+        client.publish('python/AA171/socket', 'Bogie 170010020')
+        client.publish('python/AA171/email', 'Bogie 170010020')
+        client.publish('python/BA171/mqtt', 'Bogie 170010020')
+        client.publish('python/BA171/socket', 'Bogie 170010020')
+        client.publish('python/BA171/email', 'Bogie 170010020')
+
 except KeyboardInterrupt:
     client.disconnect()
     client.loop_stop()
